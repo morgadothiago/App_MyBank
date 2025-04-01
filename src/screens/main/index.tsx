a6e5969f6c3dcd397664e44dc8f0099f348b7c13
thiago morgado <thiago_morgado@hotmail.com>
2025-03-31 23:03:00 -0300

    Inicial Commit

---
+++ src/screens/main/index.tsx
@@ -0,0 +1,53 @@
+import { Text, View } from "react-native";
+import {
+  Container,
+  Header,
+  Content,
+  IconArea,
+  UserWapper,
+  IconServices,
+  Button,
+  UserInfo,
+  UserName,
+  ContainerHeader,
+} from "./styles";
+import IconUserSVG from "../../../assets/svg/icon-user";
+import IconQUestionSVG from "../../../assets/svg/question";
+import IconOcultSVG from "../../../assets/svg/ocult";
+import IconEmailSVG from "../../../assets/svg/email";
+import { useState } from "react";
+
+export default function Home() {
+  const [name, setName] = useState("Thiago do nascimento morgado");
+  return (
+    <Container>
+      <Header>
+        <ContainerHeader>
+          <IconArea>
+            <UserWapper>
+              <IconUserSVG width={32} height={32} />
+            </UserWapper>
+
+            <IconServices>
+              <Button>
+                <IconOcultSVG />
+              </Button>
+              <Button>
+                <IconQUestionSVG />
+              </Button>
+              <Button>
+                <IconEmailSVG />
+              </Button>
+            </IconServices>
+          </IconArea>
+
+          <UserInfo>
+            <UserName>Ola, {name}</UserName>
+          </UserInfo>
+        </ContainerHeader>
+      </Header>
+
+      <Content></Content>
+    </Container>
+  );
+}
---
+++ src/screens/main/styles.ts
@@ -0,0 +1,46 @@
+import styled from "styled-components/native";
+import { colors } from "../../themes/theme";
+
+export const Container = styled.SafeAreaView`
+  flex: 1;
+`;
+export const Header = styled.View`
+  background-color: ${colors.primary};
+  height: 25%;
+  justify-content: center;
+  align-items: center;
+`;
+export const ContainerHeader = styled.View``;
+
+export const Content = styled.View`
+  background-color: ${colors.secondary};
+  flex: 1;
+`;
+export const IconArea = styled.View`
+  width: 85%;
+  flex-direction: row;
+  justify-content: space-between;
+  height: 60px;
+`;
+export const UserWapper = styled.TouchableOpacity`
+  background-color: rgba(255, 255, 255, 0.13);
+  width: 64px;
+  height: 64px;
+  border-radius: 50px;
+  align-items: center;
+  justify-content: center;
+`;
+export const IconServices = styled.View`
+  width: 150px;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+export const Button = styled.TouchableOpacity``;
+
+export const UserInfo = styled.View``;
+export const UserName = styled.Text`
+  width: 85%;
+
+  background-color: red;
+`;
